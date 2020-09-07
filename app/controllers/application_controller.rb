16b2e3c8d23bff67d6f321939e80856df8bde58b
koji149 <koji06099@gmail.com>
2020-09-07 23:43:51 +0900

    privacy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :redirect_root_domain
+
+  private
+
+  def redirect_root_domain
+    return unless request.host === 'fullfii.com'
+    redirect_to("#{request.protocol}www.fullfii.com#{request.fullpath}", status: 301)
+  end
+
 end
